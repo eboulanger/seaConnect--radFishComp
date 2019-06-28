886a905350528ddfa7980de504c81dc594f3c2e2
eboulanger <emilie.boulanger@hotmail.com>
2019-06-28 11:32:21 +0200

    deleted old files

--- admixture/convert_vcf.sh
+++
@@ -1,9 +0,0 @@
-
-# convert vcf file to plink .bed : two steps
-# .vcf to .tped
-vcftools --vcf mullus.vcf --plink-tped --out mullus
-
-# .tped to .bed
-plink --tped mullus.tped --tfam mullus.tfam --make-bed --out mullus --noweb
-
-# merci Pierre-Edouard
